496f5584495a7391cbf56ab0a076b649750e1611
jonatanfrederiksen <Jonatan3230@gmail.com>
2021-06-01 17:38:02 +0200

    touch controls

--- TETRIS/app.js
+++ TETRIS/app.js
@@ -808,6 +808,57 @@ startBtn.addEventListener('click', function () { startGame() })
 
 
 
+var touchstartX = 0;
+var touchstartY = 0;
+var touchendX = 0;
+var touchendY = 0;
+
+var gesuredZone = document.getElementById('game');
+
+gesuredZone.addEventListener('touchstart', function (e) {
+    touchstartX = e.changedTouches[0].screenX;
+    touchstartY = e.changedTouches[0].screenY;
+}, false);
+
+gesuredZone.addEventListener('touchend', function (e) {
+    touchendX = e.changedTouches[0].screenX;
+    touchendY = e.changedTouches[0].screenY;
+    handleGesure();
+}, false);
+
+function handleGesure() {
+    var swiped = 'swiped: ';
+    if (touchendX < touchstartX) {
+        alert(swiped + 'left!');
+    }
+    if (touchendX > touchstartX) {
+        alert(swiped + 'right!');
+    }
+    if (touchendY < touchstartY) {
+        alert(swiped + 'down!');
+    }
+    if (touchendY > touchstartY) {
+        alert(swiped + 'up!');
+    }
+    if (touchendY == touchstartY) {
+        alert('tap!');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
